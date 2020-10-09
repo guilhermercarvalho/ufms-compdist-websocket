3a8e6ed587c4b966be96e768eeb0963ea272b848
guilhermercarvalho <guilhermercarvalho512@gmail.com>
2020-10-09 13:09:03 +0000

    Correction: correctly renamed folders

--- app/script/alias.sh
+++ app/script/alias.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
 # Add alias to startup fast the process of compile and run
-echo "alias serexec=\"reset; javac /workspaces/java-test/src/*.java -d /workspaces/java-test/bin; cd /workspaces/java-test/bin; java Servidor; cd -\"" | tee -a ~/.bashrc ~/.zshrc >> /dev/null
-echo "alias cliexec=\"reset; cd /workspaces/java-test/bin; java Cliente; cd -\"" | tee -a ~/.bashrc ~/.zshrc >> /dev/null
+PATH_APP_SRC=/workspaces/websocket-compdist/app/src
+PATH_APP_BIN=/workspaces/websocket-compdist/app/bin
+
+echo -n "\n" | tee -a ~/.bashrc ~/.zshrc >> /dev/null
+
+echo "alias serexec=\"reset; javac ${PATH_APP_SRC}/*.java -d \
+${PATH_APP_BIN}; cd ${PATH_APP_BIN}; java Servidor; cd -\"" | tee -a ~/.bashrc ~/.zshrc >> /dev/null
+
+echo "alias cliexec=\"reset; cd ${PATH_APP_BIN}; java Cliente; cd -\"" | tee -a ~/.bashrc ~/.zshrc >> /dev/null
